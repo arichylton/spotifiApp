97af1c2766f5193cd482361ebb6b9aa8751f8bdf
Aric Hylton <arich212@gmail.com>
2023-04-19 13:51:29 -0500

    adjusted sizing of track items and changed url due to spotify guidance

--- client/src/components/TrackItem.js
+++ client/src/components/TrackItem.js
@@ -22,7 +22,7 @@ const TrackItem = ({ track, handlePlay, size, url }) => {
         </div>
       </div>
 
-      <div className='text-muted m-3'>{formatDuration(track?.duration_ms)}</div>
+      <div className='text-muted m-3 ms-auto'>{formatDuration(track?.duration_ms)}</div>
     </div>
   );
 };
